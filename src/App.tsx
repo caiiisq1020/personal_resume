bf24e3ed7fb238f678c17350ae2242d42a9457e2
zhangshaocong <2082926102@qq.com>
2026-07-15 16:38:40 +0800

    feat: wire up routing, global styles, and all pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,30 @@
-function App() {
-  return (
-    <div className="min-h-screen bg-navy text-slate font-sans antialiased">
-      <h1 className="text-offwhite text-4xl font-bold p-8">Hello World</h1>
-    </div>
-  )
-}
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
+import HomePage from './pages/HomePage';
+import ResearchPage from './pages/ResearchPage';
+import SkillsPage from './pages/SkillsPage';
+import InternshipPage from './pages/InternshipPage';
+import ProjectsPage from './pages/ProjectsPage';
+import ResumePage from './pages/ResumePage';
 
-export default App
+export default function App() {
+  return (
+    <BrowserRouter>
+      <div className="min-h-screen flex flex-col">
+        <Navbar />
+        <main className="flex-1">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/research" element={<ResearchPage />} />
+            <Route path="/skills" element={<SkillsPage />} />
+            <Route path="/internship" element={<InternshipPage />} />
+            <Route path="/projects" element={<ProjectsPage />} />
+            <Route path="/resume" element={<ResumePage />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </BrowserRouter>
+  );
+}
